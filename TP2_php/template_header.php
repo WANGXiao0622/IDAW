ad7c17ac0a88a4638e64b535d9b0f26ece12d782
WANGXiao0622 <59645356+WANGXiao0622@users.noreply.github.com>
2021-03-10 11:46:05 +0100

    php

---
+++ TP2_php/template_header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Hector Durand</title>
+            <link rel="stylesheet" href="styles.css" type="text/css" media="screen" title="default" charset="utf-8" />
+    </head>
+<body>
+    <?php
+    require_once('template_header.php'); 
+    ?>
+    
+</body>
+
+
+</html>
