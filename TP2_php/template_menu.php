a8d0c3f8771d0d3cca784d84207874e2362daf63
WANGXiao0622 <59645356+WANGXiao0622@users.noreply.github.com>
2021-03-11 23:46:41 +0100

    index

--- TP2_php/template_menu.php
+++ TP2_php/template_menu.php
@@ -1,16 +1,15 @@
 <?php
-/*
-<nav class="bloc_type">
-    <ul>
-        <li><a href="index.php">Accueil</a></li>
-        <li><a href="cv.php">Cv</a></li>
-        <li><a href="hobbies.php">My hobbies</a></li>
-    </ul> 
-</nav>
-    */
+    <div class="bloc_type">
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="cv.php">Cv</a></li>
+            <li><a href="hobbies.php">My hobbies</a></li>
+        </ul> 
+</div>   
 ?>
 
 <?php
+/*
     function renderMenuToHTML($currentPageId) {
     // un tableau qui d\'efinit la structure du site
         $mymenu = array( // idPage titre
@@ -21,14 +20,14 @@
         foreach($mymenu as $pageId => $pageParameters){
             if($pageId='index'){
                 echo "
-                <div class="bloc_type">
-                    <ul>
-                        <li><a id="currentpage" href="css_index.html">Accueil</a></li>
-                        <li><a href="css_cv.html">my CV</a></li>
-                        <li><a href="css_hobbies.html">My hobbies</a></li>
-                    </ul>
-                </div>
-                "
+                    <div class="bloc_type">
+                        <ul>
+                            <li><a id="currentpage" href="css_index.html">Accueil</a></li>
+                            <li><a href="css_cv.html">my CV</a></li>
+                            <li><a href="css_hobbies.html">My hobbies</a></li>
+                        </ul>
+                    </div>
+                    "
             }
             if($pageId='cv'){
                 echo "
@@ -56,4 +55,5 @@
         }
 
     }
+    */
 ?>
