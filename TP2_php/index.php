28ab083f8138f9cb2d8760593a0a8e66bd24293c
WANGXiao0622 <59645356+WANGXiao0622@users.noreply.github.com>
2021-03-12 00:25:39 +0100

    last

--- TP2_php/index.php
+++ TP2_php/index.php
@@ -1,27 +1,11 @@
-<?php
-/*
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Hello xiao</title>
-        <link rel="stylesheet" href="styles.css" title ="test" charset="utf-8">
-        
 
-    </head>
-
-    <body>
-*/
-    require_once('template_header.php'); 
-    ?>
+<?php require_once('template_header.php');?>
 
         <div class="page">
 
             <div class="title_type">
                 <h1 class="title">WANG Xiao</h1>
-                <a href="https://www.linkedin.com/in/xiao-wang-0b258618b/"><img src="/Users/wangxiao/IDAW/TP2_php/linkedin.png" style="float:right" width="128" height="64">
-                
-                </a>
+                <a href="https://www.linkedin.com/in/xiao-wang-0b258618b/"><img src="/Users/wangxiao/IDAW/TP2_php/linkedin.png" style="float:right" width="128" height="64"></a>
             </div>
 
             <?php
@@ -35,16 +19,28 @@
             </div>
             */
                 require_once('template_menu.php');
-                /*
-                renderMenuToHTML('index');
-                */
+                $currentPageId = 'accueil';
+                if(isset($_GET['page'])) { 
+                    $currentPageId = $_GET['page'];
+                }
 
+                renderMenuToHTML('index');
             ?>
 
             <div class="corpus">
                 <h2>Who am I?</h2>
                 <p>  A student in IMT LILLE DOUAI who know nothing about web developement</p>
             </div>
+
+            <section class="corpus">
+                <?php
+                    $pageToInclude = $currentPageId . ".php";
+                    if(is_readable($pageToInclude)) 
+                        require_once($pageToInclude);
+                    else 
+                    require_once("error.php");
+                ?>
+
 
             <?php
             /*
