d813d9db769292b723b1444693acf020b15ccc8a
WANGXiao0622 <59645356+WANGXiao0622@users.noreply.github.com>
2021-03-12 12:44:50 +0100

    final3

--- TP2_php/cv.php
+++ TP2_php/cv.php
@@ -1,13 +1,12 @@
 
 <?php require_once('template_header.php');?>
 
-        <div class="page">
 
             <div class="title_type">
                 <h1 class="title">WANG Xiao</h1>
                 <a href="https://www.linkedin.com/in/xiao-wang-0b258618b/">
                     <img src="/Users/wangxiao/IDAW/TP2_php/linkedin.png" style="float:right" width="128" height="64">
-                    
+
                 </a>
             </div>
 
@@ -27,9 +26,3 @@
             </div>
 
             <?php require_once('template_footer.php'); ?>
-
-        </div>
-
-    </body>
-
-</html>
