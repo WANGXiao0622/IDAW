e5c56a1401d8cacd1ef7d6dbd58621527da2d414
WANGXiao0622 <59645356+WANGXiao0622@users.noreply.github.com>
2021-03-12 10:03:49 +0100

    change

--- TP2_php/cv.php
+++ TP2_php/cv.php
@@ -1,19 +1,5 @@
-<?php
-/*
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Hello xiao</title>
-        <link rel="stylesheet" href="styles.css" title ="test" charset="utf-8">
-        
 
-    </head>
-
-    <body>
-    */
-        require_once('template_header.php');
-    ?>
+<?php require_once('template_header.php');?>
 
         <div class="page">
 
@@ -23,16 +9,15 @@
             </div>
 
             <?php
-            /*
-            <div class="bloc_type">
-                <ul class="menu">
-                    <li><a href="index.php">Accueil</a></li>
-                    <li><a href="cv.php">My CV</a></li>
-                    <li><a href="hobbies.php">My hobbies</a></li>
-                </ul>
-            </div>
-            */
-                require_once('template_menu.php'); 
+                require_once('template_menu.php');
+                $currentPageId = 'accueil';
+                if(isset($_GET['page'])) { 
+                    $currentPageId = $_GET['page'];
+                }
+                renderMenuToHTML($currentPageId );
+            ?>
+
+                
             ?>
 
             <div class="corpus">
@@ -41,15 +26,7 @@
                 <img src="/Users/wangxiao/IDAW/TP2_php/5.jpeg" alt="ice burge" style="float:left" width="256" height="512">
             </div>
 
-            <?php
-            /*
-            <div class="footer_type">
-                site realized by html and css
-            </div>
-            */
-                require_once('template_footer.php'); 
-            ?>
-
+            <?php require_once('template_footer.php'); ?>
 
         </div>
 
