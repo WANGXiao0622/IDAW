4f88437da426e1b46eb012564810ac7c6f9c9ba1
WANGXiao0622 <59645356+WANGXiao0622@users.noreply.github.com>
2021-03-12 10:09:34 +0100

    1

--- TP2_php/cv.php
+++ TP2_php/cv.php
@@ -22,7 +22,7 @@
             <div class="corpus">
                 <h2>My CV:</h2>
 
-                <img src="/Users/wangxiao/IDAW/TP2_php/5.jpeg" alt="ice burge" style="float:left" width="256" height="512">
+                <img src="/Users/wangxiao/IDAW/TP2_php/cv.jpeg" alt="ice burge" style="float:left" width="256" height="512">
             </div>
 
             <?php require_once('template_footer.php'); ?>
