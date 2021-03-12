645de178a8e0b10fa4e4134dfaf95aa437858671
WANGXiao0622 <59645356+WANGXiao0622@users.noreply.github.com>
2021-03-12 10:07:52 +0100

    cv

--- TP2_php/index.php
+++ TP2_php/index.php
@@ -5,7 +5,9 @@
 
             <div class="title_type">
                 <h1 class="title">WANG Xiao</h1>
-                <a href="https://www.linkedin.com/in/xiao-wang-0b258618b/"><img src="/Users/wangxiao/IDAW/TP2_php/linkedin.png" style="float:right" width="128" height="64"></a>
+                <a href="https://www.linkedin.com/in/xiao-wang-0b258618b/">
+                    <img src="/Users/wangxiao/IDAW/TP2_php/linkedin.png" style="float:right" width="128" height="64">
+                </a>
             </div>
 
             <?php
@@ -17,12 +19,7 @@
 
                 renderMenuToHTML($currentPageId );
             ?>
-<!--
-            <div class="corpus">
-                <h2>Who am I?</h2>
-                <p>  A student in IMT LILLE DOUAI who know nothing about web developement</p>
-            </div>
--->
+
             <section class="corpus">
                 <?php
                     $pageToInclude = $currentPageId . ".php";
@@ -32,15 +29,7 @@
                     require_once("error.php");
                 ?>
 
-
-            <?php
-            /*
-            <div class="footer_type">
-                site realized by html and css
-            </div>
-            */
-                require_once('template_footer.php'); 
-            ?>
+            <?php require_once('template_footer.php'); ?>
 
 
         </div>
