0f48692b7528093da50d88f3b8f51cedd4f7bfb0
WANGXiao0622 <59645356+WANGXiao0622@users.noreply.github.com>
2021-03-12 09:55:22 +0100

    function

--- TP2_php/template_menu.php
+++ TP2_php/template_menu.php
@@ -1,4 +1,4 @@
-
+<!--
 <div class="bloc_type">
     <ul>
         <li><a href="index.php">Accueil</a></li>
@@ -6,54 +6,27 @@
         <li><a href="hobbies.php">My hobbies</a></li>
     </ul> 
 </div>   
-
+-->
 
 <?php
-/*
     function renderMenuToHTML($currentPageId) {
-    // un tableau qui d\'efinit la structure du site
-        $mymenu = array( // idPage titre
-            'index' => array( 'accueil' ), 
-            'cv' => array( 'cv' ),
-            'hobbies' => array('hobbies') 
+        // un tableau qui definit la structure du site
+        $mymenu = array(
+        // idPage titre
+            'index' => array('Accueil'),
+            'cv' => array('Cv'),
+            'hobbies' => array('My hobbies')  
         );
-        foreach($mymenu as $pageId => $pageParameters){
-            if($pageId='index'){
-                echo "
-                    <div class="bloc_type">
-                        <ul>
-                            <li><a id="currentpage" href="css_index.html">Accueil</a></li>
-                            <li><a href="css_cv.html">my CV</a></li>
-                            <li><a href="css_hobbies.html">My hobbies</a></li>
-                        </ul>
-                    </div>
-                    "
+        echo '<div class="bloc_type">'
+        echo "<ul>";
+        foreach($mymenu as $pageId => $pageParameters) {
+            if ($pageId == $currentPageId) {
+                echo "<li><a id='currentpage' href='" . $pageId . ".php'>" . $pageParameters[0] . "</a></li>";
+            } else {
+                echo "<li><a href='" . $pageId . ".php'>" . $pageParameters[0] . "</a></li>";
             }
-            if($pageId='cv'){
-                echo "
-                <div class="bloc_type">
-                    <ul>
-                        <li><a href="css_index.html">Accueil</a></li>
-                        <li><a id="currentpage" href="css_cv.html">my CV</a></li>
-                        <li><a href="css_hobbies.html">My hobbies</a></li>
-                    </ul>
-                </div>
-                "
-            }
-            if($pageId='hobbies'){
-                echo "
-                <div class="bloc_type">
-                    <ul>
-                        <li><a href="css_index.html">Accueil</a></li>
-                        <li><a href="css_cv.html">my CV</a></li>
-                        <li><a id="currentpage" href="css_hobbies.html">My hobbies</a></li>
-                    </ul>
-                </div>
-                "
-            }
-            
         }
-
-    }
-    */
+        echo "</ul>";
+        echo "</div>";
+}
 ?>
