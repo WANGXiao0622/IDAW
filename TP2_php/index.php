53a4706d41bc767d6a9dd31eb2729a4b8db4344b
WANGXiao0622 <59645356+WANGXiao0622@users.noreply.github.com>
2021-03-12 10:59:47 +0100

    img

--- TP2_php/index.php
+++ TP2_php/index.php
@@ -5,14 +5,12 @@
 
             <div class="title_type">
                 <h1 class="title">WANG Xiao</h1>
-                <a href="https://www.linkedin.com/in/xiao-wang-0b258618b/">
-                    <img src="/Users/wangxiao/IDAW/TP2_php/linkedin.png" style="float:right" width="128" height="64">
-                </a>
+                <a href="https://www.linkedin.com/in/xiao-wang-0b258618b/"><img src="/Users/wangxiao/IDAW/TP2_php/linkedin.png" style="float:right" width="128" height="64">
             </div>
 
             <?php
                 require_once('template_menu.php');
-                $currentPageId = 'accueil';
+                $currentPageId = 'index';
                 if(isset($_GET['page'])) { 
                     $currentPageId = $_GET['page'];
                 }
