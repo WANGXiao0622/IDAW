bc8add1902859c3ee9ba96db8e29e7ef9cfa6b53
WANGXiao0622 <59645356+WANGXiao0622@users.noreply.github.com>
2021-03-12 11:01:17 +0100

    a

--- TP2_php/index.php
+++ TP2_php/index.php
@@ -5,7 +5,9 @@
 
             <div class="title_type">
                 <h1 class="title">WANG Xiao</h1>
-                <a href="https://www.linkedin.com/in/xiao-wang-0b258618b/"><img src="/Users/wangxiao/IDAW/TP2_php/linkedin.png" style="float:right" width="128" height="64">
+                <a href="https://www.linkedin.com/in/xiao-wang-0b258618b/">
+                    <img src="/Users/wangxiao/IDAW/TP2_php/linkedin.png" style="float:right" width="128" height="64">
+                </a>
             </div>
 
             <?php
