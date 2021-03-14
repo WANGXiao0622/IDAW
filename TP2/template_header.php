205161bcf02948ccc23b23ac5ab78f4358399576
WANGXiao0622 <59645356+WANGXiao0622@users.noreply.github.com>
2021-03-14 18:07:20 +0100

    title

--- TP2/template_header.php
+++ TP2/template_header.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>WANG Xiao</title>
-            <link rel="stylesheet" href="styles.css" type="text/css" media="screen" title="default" charset="utf-8" />
+            <link rel="stylesheet" href="style1.css" type="text/css" media="screen" title="default" charset="utf-8" />
     </head>
 
 <body>   
