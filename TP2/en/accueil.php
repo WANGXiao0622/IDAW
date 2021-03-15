6a59a39eefa82db7e22776136073a70937bcb930
WANGXiao0622 <59645356+WANGXiao0622@users.noreply.github.com>
2021-03-15 15:07:07 +0100

    final

--- TP2/en/accueil.php
+++ TP2/en/accueil.php
@@ -2,7 +2,8 @@
 require_once "template_header.php"
 ?>
     <div class="corpus">
-        <h2>Welcome to my page!</h2>
+        <h1>Welcome to my page!</h1>
+        <hr>
         <h2>Who am I?</h2>
             <p>  A student in IMT LILLE DOUAI who know nothing about web developement</p>
     </div>
--- TP2/fr/accueil.php
+++ TP2/fr/accueil.php
@@ -2,7 +2,8 @@
 require_once "template_header.php"
 ?>
     <div class="corpus">
-        <h2> Bienvenue sur mon site professionel! </h2>
+        <h1> Bienvenue sur mon site professionel! </h1>
+        <hr>
         <h2>Qui suis je?</h2>
             <p>  Un étudiant en IMT LILLE DOUAI qui ne sait rien du développement web</p>
     </div>
